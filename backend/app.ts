5a98b088f70e109ec9fd92e9dae1476f9700b7a5
Mark Tasaka <mark.tasaka@gmail.com>
2024-03-19 15:55:46 -0700

    Add files via upload

---
+++ backend/app.ts
@@ -0,0 +1,86 @@
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import {
+  findUserById,
+  IDecodedUser,
+  verifyUser,
+  parseToken,
+  addPost,
+  posts,
+  sleep,
+} from "./fakedb";
+
+
+//Mark Tasaka
+
+const port = 8085;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+// TODO: Obviously use a more secure signing key than "secret"
+app.post("/api/user/login", (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = verifyUser(email, password);
+    const token = jwt.sign({ id: user.id }, "secret", {
+      expiresIn: "2 days",
+    });
+    res.json({ result: { user, token } });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+app.post("/api/user/validation", (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = parseToken(authHeader, res);
+    const decodedUser = jwt.verify(token, "secret");
+    const user = findUserById((decodedUser as IDecodedUser).id);
+    res.json({ result: { user, token } });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+app.get("/api/posts", async (req, res) => {
+  // Sleep delay goes here
+  //3 second delay
+  //await sleep(3000);
+  //Demo();
+  
+  res.json(posts);
+});
+
+// ⭐️ TODO: Implement this yourself
+app.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id; // "2"
+  // The line below should be fixed.
+  const post = posts[Number(id) - 1];
+  // @ts-ignore
+  post["author"] = (findUserById(Number(id))).email.split("@")[0];
+  res.json(post);
+});
+
+/**
+ * Problems with this:
+ * (1) Authorization Issues:
+ *     What if you make a request to this route WITHOUT a token?
+ *     What if you make a request to this route WITH a token but
+ *     it's invalid/expired?
+ * (2) Server-Side Validation Issues:
+ *     What if you make a request to this route with a valid token but
+ *     with an empty/incorrect payload (post)
+ */
+app.post("/api/posts", (req, res) => {
+  const incomingPost = req.body;
+  addPost(incomingPost);
+  res.status(200).json({ success: true });
+});
+
+app.listen(port, () => console.log("Server is running"));
